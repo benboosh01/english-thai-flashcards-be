8c125dc96c61c16e74e42b0c42e58a4bba7f3169
Ben Bushell <ben.bushell87@icloud.com>
2022-12-09 16:44:18 +0700

    add cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const controllers = require("./controllers/thai-flashcard.controllers");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/api/phrases", controllers.getAllPhrases);
