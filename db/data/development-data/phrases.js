e1a619d0c7e0b36d20baefe0c5cdb987cab00d53
Ben Bushell <ben.bushell87@icloud.com>
2022-12-09 16:10:42 +0700

    update for production deployment

---
+++ db/data/development-data/phrases.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    english: "this item",
+    thai: "อันนี้ (an nii)",
+  },
+  {
+    english: "how much",
+    thai: "เท่าไหร่ (tao rai)",
+  },
+  {
+    english: "What is your name?",
+    thai: "คุณชื่ออะไร (khun cheu ah-rai)",
+  },
+  {
+    english: "My name is _____",
+    thai: "ผม ชื่อ ____ (phom (male) chue ____)",
+  },
+  {
+    english: "I’ll have 1 bottle of beer",
+    thai: "เอา เบียร์ หนึ่ง  ขวด (Ow bia nueng khuat)",
+  },
+  {
+    english: "Can I have the menu please?",
+    thai: "ขอเมนูหน่อย (khaw meh-nuu noi)",
+  },
+  {
+    english: "I’m ready to order",
+    thai: "พร้อมสั่ง (phrawm sang)",
+  },
+  {
+    english: "cheers",
+    thai: "ชนแก้ว (chon kaew)",
+  },
+  {
+    english: "good luck",
+    thai: "โชคดี (chohk dee)",
+  },
+];
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,22 +1,24 @@
 const format = require("pg-format");
 const db = require("../connection");
+const uuid = require("uuid");
 
 const seed = async ({ phrasesData }) => {
   await db.query(`DROP TABLE IF EXISTS phrases`);
 
   const phrasesTablePromise = db.query(`CREATE TABLE phrases (
-    english VARCHAR,
-    thai VARCHAR
+    id uuid PRIMARY KEY,
+    english VARCHAR NOT NULL,
+    thai VARCHAR NOT NULL
 );`);
 
   await phrasesTablePromise;
 
   const insertPhrasesQueryStr = format(
     `
-    INSERT INTO phrases (english, thai) VALUES %L RETURNING *;
+    INSERT INTO phrases (id, english, thai) VALUES %L RETURNING *;
 
   `,
-    phrasesData.map(({ english, thai }) => [english, thai])
+    phrasesData.map(({ english, thai }) => [uuid.v4(), english, thai])
   );
 
   const phrasesPromise = db
