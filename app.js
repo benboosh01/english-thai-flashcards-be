e2371b5e812b0006632f8b36463fdb883a0fb997
Ben Bushell <ben.bushell87@icloud.com>
2022-12-12 13:00:04 +0700

    add error handling

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const controllers = require("./controllers/thai-flashcard.controllers");
+const errorControllers = require("./controllers/error-handling.controllers");
 const cors = require("cors");
 
 const app = express();
@@ -9,5 +10,12 @@ app.use(express.json());
 
 app.get("/api/phrases", controllers.getAllPhrases);
 app.post("/api/phrases", controllers.postPhrase);
+
+app.all("*", (req, res) => {
+  res.status(404).send({ msg: "invalid URL" });
+});
+
+app.use(errorHandlers.customError);
+app.use(errorHandlers.serverError);
 
 module.exports = app;
---
+++ controllers/error-handling.controllers.js
@@ -0,0 +1,12 @@
+exports.customError = (err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+};
+
+exports.serverError = (err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ msg: "Internal Server Error" });
+};
