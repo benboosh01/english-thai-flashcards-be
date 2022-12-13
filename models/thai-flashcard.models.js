ccf24367967f334e708ee95bd6486e62c3caf073
Ben Bushell <ben.bushell87@icloud.com>
2022-12-13 15:35:33 +0700

    update models with new columns

--- models/thai-flashcard.models.js
+++ models/thai-flashcard.models.js
@@ -6,16 +6,16 @@ exports.selectAllPhrases = async () => {
 };
 
 exports.insertPhrase = async (newPhrase) => {
-  const { id, english, thai } = newPhrase;
-  if (!english || !thai) {
+  const { id, english, thai_script, thai_latin, category } = newPhrase;
+  if (!english || !thai_script || !thai_latin || !category) {
     return Promise.reject({
       status: 400,
       msg: "missing details - please complete all fields and resubmit",
     });
   }
 
-  const queryStr = `INSERT INTO phrases (id, english, thai) VALUES ($1, $2, $3) RETURNING *;`;
-  const queryVals = [id, english, thai];
+  const queryStr = `INSERT INTO phrases (id, english, thai_script, thai_latin, category) VALUES ($1, $2, $3, $4, $5) RETURNING *;`;
+  const queryVals = [id, english, thai_script, thai_latin, category];
   const result = await db.query(queryStr, queryVals);
   return result.rows[0];
 };
